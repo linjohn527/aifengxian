1b746859d1adf3b16e8a389358e56957d348649b
linjohn527 <linjohn@linjohns-mac.local>
2016-04-08 14:43:48 +0800

    初始化项目

---
+++ LoveBeeFresh/LoveBeeFresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoveBeeFresh
+//
+//  Created by linjohn on 4/8/16.
+//  Copyright © 2016 linjohn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
